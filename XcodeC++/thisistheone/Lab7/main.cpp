738e8ea908df50cd50e705ad3502a21b2b417f05
aedign <aedign@icloud.com>
2019-10-06 19:56:39 -0400

    file added

---
+++ XcodeC++/thisistheone/Lab7/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab7
+//
+//  Created by Andres Di Gregorio on 10/6/19.
+//  Copyright © 2019 Andres Di Gregorio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
